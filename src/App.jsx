4a307da53b587d16febed7ca31d520ac528fb49a
sahilthedeveloper <sahilshekh1117@gmail.com>
2024-07-25 16:16:52 +0530

    sahil shekh portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import NavBar from "./components/NavBar";
 const App = () => {
   return (
     <main className="bg-slate-300/20 h-full">
-      <Router>
+      <Router basename="/sahilshekh">
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
+    assetsDir: "assets",
   },
   assetsInclude: ["**/*.glb"],
 });
